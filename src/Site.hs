29c17a1ef540225d0a29ab71ecb86e8e48cfcfa3
Jan Stolarek <jan.stolarek@ed.ac.uk>
2021-10-26 11:52:09 +0100

    Remove unused code
    Closes #7

--- src/Site.hs
+++ src/Site.hs
@@ -1,9 +1,9 @@
---------------------------------------------------------------------------------
 {-# LANGUAGE OverloadedStrings #-}
+module Main where
+
 import           Data.Monoid (mappend)
 import           Hakyll
 
---------------------------------------------------------------------------------
 main :: IO ()
 main = hakyllWith config $ do
     match "images/*" $ do
@@ -47,42 +47,6 @@ main = hakyllWith config $ do
             >>= loadAndApplyTemplate "templates/post.html"    postCtx
             >>= loadAndApplyTemplate "templates/default.html" postCtx
             >>= relativizeUrls
-
-{-
-    create ["index.html"] $ do
-        route $ setExtension "html"
-        compile $ pandocCompiler
-            >>= loadAndApplyTemplate "templates/default.html" defaultContext
-            >>= relativizeUrls
-    create ["archive.html"] $ do
-        route idRoute
-        compile $ do
-            posts <- recentFirst =<< loadAll "posts/*"
-            let archiveCtx =
-                    listField "posts" postCtx (return posts) `mappend`
-                    constField "title" "Archives"            `mappend`
-                    defaultContext
-
-            makeItem ""
-                >>= loadAndApplyTemplate "templates/archive.html" archiveCtx
-                >>= loadAndApplyTemplate "templates/default.html" archiveCtx
-                >>= relativizeUrls
--}
-
-{-
-    match "index.html" $ do
-        route idRoute
-        compile $ do
-            posts <- recentFirst =<< loadAll "posts/*"
-            let indexCtx =
-                    listField "posts" postCtx (return posts) `mappend`
-                    defaultContext
-
-            getResourceBody
-                >>= applyAsTemplate indexCtx
-                >>= loadAndApplyTemplate "templates/default.html" indexCtx
-                >>= relativizeUrls
--}
 
     match "templates/*" $ compile templateBodyCompiler
 
